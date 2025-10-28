2cee7aa917fd7d065c4387182c0f7832a4fcfff2
Mazahir Haroon <mazahirharoon@gmail.com>
2025-10-28 22:22:39 +0530

    refactor: rename main object to ekmLibrary and simplify display logic for Day 2

--- ObjectAndArrays/LibraryManagementSystem/Day2/script.js
+++ ObjectAndArrays/LibraryManagementSystem/Day2/script.js
@@ -3,7 +3,7 @@ Notes: https://any.coop/AB2DLqz9p8Fk7KCdSnkivxZPhPYw9K3ecArpX5jqcEj7Yuga/day-2-l
 */
 
 // Our main object — represents the Library system
-const library = {
+const ekmLibrary = {
   name: 'Ernakulam Public Library',
   location: 'Convent Rd, Kochi, Ernakulam',
   totalBooks: 200000,
@@ -26,14 +26,15 @@ document.getElementById('submitBtn').addEventListener('click', function () {
   };
 
   // Step 3: Push this new object into the 'borrowedBooks' array inside 'library' object
-  library.borrowedBooks.push(borrowedBook);
+  ekmLibrary.borrowedBooks.push(borrowedBook);
 
   // Step 4: Show the updated object structure on the page
-  document.getElementById('output').textContent = JSON.stringify(
-    library,
-    null,
-    2
-  );
+  // document.getElementById('output').textContent = JSON.stringify(
+  //   ekmLibrary,
+  //   null,
+  //   2
+  // );
+  console.log(ekmLibrary);
 
   // Step 5: Clear inputs
   document.getElementById('bookTitle').value = '';
@@ -50,7 +51,7 @@ function displayBorrowedBooks() {
   list.innerHTML = ''; // clear old list
 
   // Loop through borrowedBooks array
-  library.borrowedBooks.forEach(function (book, index) {
+  ekmLibrary.borrowedBooks.forEach(function (book, index) {
     const listItem = document.createElement('li');
     listItem.textContent = `#${index + 1} — "${book.title}" borrowed by ${
       book.borrower
