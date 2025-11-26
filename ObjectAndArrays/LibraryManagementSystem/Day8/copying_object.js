d2fb63cca61e765e82b2e6a5c10679afa5baffff
Mazahir Haroon <mazahirharoon@gmail.com>
2025-11-26 23:32:06 +0530

    feat: add examples comparing object references and value copying in JavaScript in Day 8 directory

---
+++ ObjectAndArrays/LibraryManagementSystem/Day8/copying_object.js
@@ -0,0 +1,22 @@
+const book = {
+  bookTitle: 'Alchemist',
+  available: true,
+};
+
+const anotherBook = {
+  bookTitle: 'Alchemist',
+  available: true,
+};
+
+console.log(book);
+console.log(anotherBook);
+
+console.log(book === anotherBook);
+
+let name = 'Naveen';
+const anotherName = name;
+
+name = 'Mazahir';
+
+console.log(name);
+console.log(anotherName);
