7ad5d955c5fff4e9a6ca41689f6807ce665af249
Mazahir Haroon <mazahirharoon@gmail.com>
2025-12-03 23:14:07 +0530

    docs(asyncjs/script): add day-2 live sandbox examples for callbacks

--- AsyncJS/script.js
+++ AsyncJS/script.js
@@ -1,16 +1,18 @@
-// main, makePayment, print
+// Memory -> main, makePayment, print
+// 1. Memory Phase 2. Execution Phase
 
 main();
 
 function main() {
   function print() {
-    console.log('Payment Done');
+    console.log('Print Bill');
   }
-
   makePayment(print);
 }
 
 function makePayment(f) {
-  // making the payment
+  console.log('Payment Started');
+  // // Data Fetching - Browser please take care of this and let me know when it ends.
+  console.log('Payment Completed');
   f();
 }
