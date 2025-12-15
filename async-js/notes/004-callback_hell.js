a8751092199671e7aef747606edd916a7db03948
Mazahir Haroon <mazahirharoon@gmail.com>
2025-12-15 23:30:22 +0530

    feat(async-js): clean up callback hell example for clearer execution flow

--- async-js/notes/004-callback_hell.js
+++ async-js/notes/004-callback_hell.js
@@ -1,59 +1,8 @@
 authenticateUser(fetchUserProfile, fetchUserNotifications, renderDashboard);
-// fetchUserProfile();
-// fetchUserNotifications();
-// renderDashboard();
 
 // --------------------
 // FUNCTIONS
 // --------------------
-
-// function authenticateUser() {
-//   console.log('--------------------');
-//   console.log('User Authentication Started');
-//   console.log('--------------------');
-//   setTimeout(() => {
-//     console.log('--------------------');
-//     console.log('User authenticated | --> You can Fetch User Profile');
-//     console.log('--------------------');
-//   }, 8000);
-// }
-
-// function fetchUserProfile() {
-//   console.log('--------------------');
-//   console.log('User Profile Fetch Started');
-//   console.log('--------------------');
-//   setTimeout(() => {
-//     console.log('--------------------');
-//     console.log('User profile fetched | --> You can Load Notifications');
-//     console.log('--------------------');
-//   }, 3000);
-// }
-
-// function fetchUserNotifications() {
-//   console.log('--------------------');
-//   console.log('Notifications Loading Started');
-//   console.log('--------------------');
-//   setTimeout(() => {
-//     console.log('--------------------');
-//     console.log('Notifications loaded | --> You can Render Dashboard');
-//     console.log('--------------------');
-//   }, 5000);
-// }
-
-// function renderDashboard() {
-//   console.log('--------------------');
-//   console.log('Rendering dashboard Started');
-//   console.log('--------------------');
-//   setTimeout(() => {
-//     console.log('--------------------');
-//     console.log('Dashboard ready');
-//     console.log('--------------------');
-//   }, 2000);
-// }
-
-/*.............................*/
-
-authenticateUser(fetchUserProfile, fetchUserNotifications, renderDashboard);
 
 function authenticateUser(
   fetchUserProfile,
