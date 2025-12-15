47fddb84a9b7d52fcfe2039f3b6a7e16e50748b5
Mazahir Haroon <mazahirharoon@gmail.com>
2025-12-15 23:35:03 +0530

    docs(async-js): add live sandbox examples for creating promises and handling with .then and .catch

--- async-js/script.js
+++ async-js/script.js
@@ -1,28 +1,45 @@
+fetch('https://jsonplaceholder.typicode.com/todos')
+  .then((c) => {
+    return c.json();
+  })
+  .then((c) => {
+    console.log(c);
+  })
+  .catch((c) => {
+    console.log('Error', c);
+  });
+
+// 1. How to create and return a promise
+function getUser() {
+  return new Promise((a, b) => {
+    if (1 === 1) {
+      a('Mazahir');
+    } else {
+      b('Something went wrong');
+    }
+  });
+}
+
+// 2. How we consume it.
+const response = getUser()
+  .then((c) => {
+    console.log('User:', c);
+  })
+  .catch((c) => {
+    console.log('Error', c);
+  });
+
 function printBill() {
   console.log('Print Bill');
 }
 
+/*
+--------------------------------------------
+*/
+// Callback Notes
+
 makePayment(printBill);
 
-/*
-.
-.
-.
-.
-.
-.
-.
-.
-.
-.
-.
-.
-.
-.
-.
-..
-
-*/
 function makePayment(printBill) {
   console.log('Payment Process Started');
 
