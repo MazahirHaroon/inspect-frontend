6828cd9782b021bdf61c611fb406492e096e04b6
Mazahir Haroon <mazahirharoon@gmail.com>
2025-12-18 15:13:50 +0530

    docs(async-js): add live sandbox examples for creating using async await with try catch

--- async-js/script.js
+++ async-js/script.js
@@ -1,16 +1,25 @@
-fetch('https://jsonplaceholder.typicode.com/todos')
-  .then((c) => {
-    return c.json();
-  })
-  .then((c) => {
-    console.log(c);
-  })
-  .catch((c) => {
-    console.log('Error', c);
-  });
+/*
+Microtask queue:
+(c) => {
+    console.log('User:', c);
+  }
+*/
 
 // 1. How to create and return a promise
 function getUser() {
+  console.log('inside main user');
+  return new Promise((a, b) => {
+    if (1 === 1) {
+      // time consuming
+      a('Mazahir');
+    } else {
+      b('Something went wrong');
+    }
+  });
+}
+
+function getUser() {
+  console.log('inside main user');
   return new Promise((a, b) => {
     if (1 === 1) {
       a('Mazahir');
@@ -21,39 +30,65 @@ function getUser() {
 }
 
 // 2. How we consume it.
-const response = getUser()
-  .then((c) => {
-    console.log('User:', c);
-  })
-  .catch((c) => {
-    console.log('Error', c);
-  });
+console.log('start');
 
-function printBill() {
-  console.log('Print Bill');
+// .then . catch
+// const response = getUser()
+//   .then((c) => {
+//     console.log('User:', c);
+//   })
+//   .catch((c) => {
+//     console.log('Error', c);
+//   });
+
+// async await
+async function main() {
+  try {
+    console.log('inside main function');
+    const user = await getUser();
+    console.log('User', user);
+  } catch (c) {
+    console.log('Error:', c);
+  }
 }
+main();
 
-/*
---------------------------------------------
-*/
+console.log('end');
+console.log('end');
+console.log('end');
+console.log('end');
+console.log('end');
+
+// --------------------------------------------
 // Callback Notes
 
-makePayment(printBill);
+// makePayment(printBill);
 
-function makePayment(printBill) {
-  console.log('Payment Process Started');
+// function makePayment(printBill) {
+//   console.log('Payment Process Started');
 
-  const xhr = new XMLHttpRequest();
-  xhr.open('GET', 'https://jsonplaceholder.typicode.com/users/1');
+//   const xhr = new XMLHttpRequest();
+//   xhr.open('GET', 'https://jsonplaceholder.typicode.com/users/1');
 
-  xhr.onload = function () {
-    console.log('Payment Completed');
-    printBill(); // print only when XHR finishes
-  };
+//   xhr.onload = function () {
+//     console.log('Payment Completed');
+//     printBill(); // print only when XHR finishes
+//   };
 
-  xhr.onerror = function () {
-    console.log('Payment failed');
-  };
+//   xhr.onerror = function () {
+//     console.log('Payment failed');
+//   };
 
-  xhr.send();
-}
+//   xhr.send();
+// }
+
+// fetch('https://jsonplaceholder.typicode.com/todos')
+//   .then((c) => {
+//     return c.json();
+//   })
+//   .then((c) => {
+//     console.log(c);
+//   })
+//   .catch((c) => {
+//     console.log('Error', c);
+//   });
